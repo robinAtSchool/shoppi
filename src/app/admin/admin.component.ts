d1c7921c8ab29c7112556414731f8b62647d310b
robinAtSchool <roebin.goeppert@gmail.com>
2017-12-12 12:01:29 +0100

    Image URL field added to product admin page

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -23,13 +23,6 @@ export class AdminComponent implements OnInit {
 
 
   addProduct() {
-    /*
-    this.newProduct.available = 123;
-    this.newProduct.brand = 'theBrand';
-    this.newProduct.description = 'My first product';
-    this.newProduct.labels = {label1: true};
-    this.newProduct.specs = {length: 34};
-    this.newProduct.name = 'The real product';*/
-    this.dbService.addProduct(this.newProduct).then();
+    this.dbService.addProduct(this.newProduct);
   }
 }
