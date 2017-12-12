a7a044e3adfca7f4b9c194f8a6f90a84112f4ccb
robinAtSchool <roebin.goeppert@gmail.com>
2017-12-12 11:37:00 +0100

    real images added

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -2,6 +2,7 @@ export class Product {
   name: string;
   brand: string;
   description: string;
+  imageUrl: string;
   labels: {[k: string]: any};
   specs: {[k: string]: any};
   available: number;
